d71d03d4199e972384c52f876866d8ec58596aad
Jakub Trmal <trmaljak@fel.cvut.cz>
2018-01-07 02:54:08 +0100

    error log file

--- semestralka/login.php
+++ semestralka/login.php
@@ -2,29 +2,50 @@
 require("php/db_lib.php");
 ob_start();
 
-function kontrola($user) {
-    echo "kontrola: ".$user;
-    // print_r(get_user($user));
-    if ($user == "kuba@trmal") {
-        return true;
-    } else {
-        return false;
-    }
+$user = "";
+$info="";
+
+if (isset($_GET["m"]) && $_GET["m"]==1) {
+    $info = "Registrace proběhla úspěšně";
 }
+
+function check($email, $password) {
+    $user_record = get_user($email);
+    // print_r($user_record);
+
+    if (is_array($user_record)) {
+        echo "1";
+        if (isset($user_record["email"]) && $user_record["email"] == $email) {
+            echo "2";
+            if (password_verify($password.$email, $user_record["password"])) {
+                echo "3";
+                return true;
+            } else return false;
+        } else return false;
+    } else {
+        false;
+    }
+    
+}
+
 // predvyplnovani formulare pokud byl uz nekdy vyplneny
 // v action vola sebe sama
-$user = "";
-$errorJmeno = ""; // datove promenne
-if (isset($_POST["user"])) {
-    $user = $_POST["user"];
-    $errorJmeno = "spatne vyplneno";
-    
-    if (kontrola($user)) {
+
+// $errorJmeno = ""; // datove promenne
+if (count(array_filter($_POST)) === 2) {
+    $email = $_POST["email"];
+    $password = $_POST["pass"];
+
+    if (check($email, $password)) {
     //     // ulozeni do DB
     //     // metodou get se presmeruje na jinou stranku - viz PRG
         // header("HTTP/1.1 303 See Other");
         header("Location: http://wa.toad.cz/~trmaljak");
         die();
+    } else {
+        if (isset($_GET["m"]) && $_GET["m"]==2) {
+            $info = "Přihlášení bylo neúspěšné, zkuste to znovu";
+        }
     }
 }
 ?>
@@ -47,13 +68,15 @@ if (isset($_POST["user"])) {
             </li>
         </ul>
     </nav>
-    <form class="login-form" method="POST" action="login.php">
+    <form class="login-form" method="POST" action="login.php?m=2">
+        <h1>Přihlášení</h1>
         <div class="container">
             <img src="images/avatar.png" alt="avatar picture">
         </div>
+        <?php echo htmlspecialchars($info)?>
         <div class="container">
             <label>Email</label>
-            <input id="email" type="email" value="<?php echo htmlspecialchars($user)?>" name="user">
+            <input id="email" type="email" value="<?php echo htmlspecialchars($user)?>" name="email">
 
             <label>Heslo</label>
             <input id="password" type="password" name="pass">
--- semestralka/registration.php
+++ semestralka/registration.php
@@ -4,7 +4,7 @@ $firstname = "";
 $user = "";
 $info = "";
 
-// ob_start();
+ob_start();
 
 /**
  * check if user exist
@@ -13,93 +13,75 @@ $info = "";
  *          false - we have record, select new one
  */
 function check($user) {
-    print_r(get_user($user));
-    if (get_user($user) == null) {
-        return true;
-    } else {
+    if (is_array(get_user($user))) {
         return false;
+    } else {
+        return true;
     }
 }
-
-// predvyplnovani formulare pokud byl uz nekdy vyplneny
-// v action vola sebe sama
-
-print_r($_POST);
 
 if (count(array_filter($_POST)) === 4) {
     $user = $_POST["user"];
     $firstname = $_POST["firstname"];
     $password = $_POST["pass"];
-    echo "jsem tu";
+
     if (check($user)) {
-        // add_user($user,$firstname,$password);
+        add_user($firstname,$user,$password);
+
+        header("Location: login.php?m=1");
+        die();
     } else {
         $info = "<h3>Email je již obsazený</h3>";
     }
-} 
-// else {
-//     $info = "<h3>Vyplňte všechny položky</h3>";
-// }
-
-
-// if (isset($_POST["user"])) {
-//     $user = $_POST["user"];
-    
-//     if (kontrola($user)) {
-//     //     // ulozeni do DB
-//     //     // metodou get se presmeruje na jinou stranku - viz PRG
-//         // header("HTTP/1.1 303 See Other");
-//         header("Location: http://wa.toad.cz/~trmaljak");
-//         die();
-//     }
-// }
+}
 ?>
-<!DOCTYPE html>
-<html lang="cs">
+    <!DOCTYPE html>
+    <html lang="cs">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Registration page</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>Registration page</title>
 
-    <link href="css/main.css" rel="stylesheet" type="text/css">
-    <link href="css/forms.css" rel="stylesheet" type="text/css">
-</head>
+        <link href="css/main.css" rel="stylesheet" type="text/css">
+        <link href="css/forms.css" rel="stylesheet" type="text/css">
+    </head>
 
-<body>
-    <nav>
-        <ul class="horizontal-nav">
-            <li class="nav-item">
-                <a href="index.html">Home</a>
-            </li>
-        </ul>
-    </nav>
-    <form class="registration-form" method="POST" action="registration.php">
-        <h2>Novy uzivatel</h2>
-        <div class="container">
-            <img src="images/avatar.png" alt="avatar picture">
-        </div>
-        <div class="container">
-            <?php echo $info?>
-            <label>Jmeno</label>
-            <input id="firstname" type="text" name="firstname" value="<?php echo htmlspecialchars($firstname)?>">
+    <body>
+        <nav>
+            <ul class="horizontal-nav">
+                <li class="nav-item">
+                    <a href="index.html">Home</a>
+                </li>
+            </ul>
+        </nav>
+        <form class="registration-form" method="POST" action="registration.php">
+            <h2>Novy uzivatel</h2>
+            <div class="container">
+                <img src="images/avatar.png" alt="avatar picture">
+            </div>
+            <div class="container">
+                <?php echo htmlspecialchars($info)?>
+                <label>Jmeno</label>
+                <input id="firstname" type="text" name="firstname" value="<?php echo htmlspecialchars($firstname)?>">
 
-            <label>Email</label>
-            <input id="email" type="email" name="user" value="<?php echo htmlspecialchars($user)?>">
+                <label>Email</label>
+                <input id="email" type="email" name="user" value="<?php echo htmlspecialchars($user)?>">
 
-            <label>Heslo</label>
-            <input id="password" type="password" name="pass">
-            
-            <label>Potvrdit heslo</label>
-            <input id="password2" type="password" name="pass2">
+                <label>Heslo</label>
+                <input id="password" type="password" name="pass">
 
-            <button type="submit">Registrovat</button>
-        </div>
-    </form>
+                <label>Potvrdit heslo</label>
+                <input id="password2" type="password" name="pass2">
+
+                <button type="submit">Registrovat</button>
+            </div>
+        </form>
 
 
-    <!-- scripts section -->
-    <script src="js/registrationForm.js"></script>
+        <!-- scripts section -->
+        <script src="js/registrationForm.js"></script>
+        <!-- <script src="js/tempAlert.js"></script> -->
 
-</body>
+    </body>
 
-</html>
+    </html>
